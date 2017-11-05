ceb5eb53fe08e96196e577c02d27e3fe1b0b423d
Enzo <me@r33int.xyz>
2017-11-05 15:28:20 +0100

    no

--- bot.py
+++ bot.py
@@ -50,9 +50,8 @@ def command_text(m):
 def command_text(m):
 	bot.send_message(m.chat.id, "Ouais en gros tu t'en blc...")
 
-@bot.message_handler(func=lambda message: "exit" in message.text.lower())
+@bot.message_handler(func=lambda message: "aok" in message.text.lower())
 def command_text(m):
-
-	bot.send_video(m.chat_id, open('issou.gif', 'rb'))
+	bot.send_video(m.chat.id, open('issou.gif', 'rb'))
 
 bot.polling(none_stop = True)
