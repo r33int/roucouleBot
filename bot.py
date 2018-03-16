e2dcbf79369cd84eb8a91aa973b29184512cc62e
r33int <r33intx@gmail.com>
2018-03-16 20:48:38 +0100

    Change handling method
    Might fix instabililties, might not...

--- bot.py
+++ bot.py
@@ -12,22 +12,22 @@ telebot.logger.setLevel(logging.INFO) # Outputs debug messages to console.
 
 @bot.message_handler(func=lambda m: 'nyo' in m.text.lower())
 def handle_test(message):
-    bot.send_message(message.chat.id, 'success')
+    bot.send_message(message.chat.id, 'eh regardez ma télé 4k comme elle est trop belle')
+
+@bot.message_handler(func=lambda m: 'hifumin' in m.text.lower())
+def handle_test(message):
+    bot.send_message(message.chat.id, '<3')
 	
-@bot.message_handler(func=lambda message: "hifumin" in message.text.lower())
-def command_text(m):
-    bot.send_message(m.chat.id, "<3")
+@bot.message_handler(func=lambda m: 'sfr' in m.text.lower())
+def handle_test(message):
+    bot.send_message(message.chat.id, 'Bonjour, votre demande concerne bien votre IBAN FR42904290489FE ?')
 
-@bot.message_handler(func=lambda message: "sfr" in message.text.lower())
-def command_text(m):
-    bot.send_message(m.chat.id, "Bonjour, votre demande concerne bien votre IBAN FR42904290489FE ?")
-
-@bot.message_handler(func=lambda message: "sdchachaze" in message.text.lower())
-def command_text(m):
+@bot.message_handler(func=lambda m: "sdchachaze" in m.text.lower())
+def command_text(message):
     bot.send_message(m.chat.id, "on voit au nom que c'est nul")
 
-@bot.message_handler(func=lambda message: "jpp" in message.text.lower())
-def command_photo(m):
+@bot.message_handler(func=lambda m: "jpp" in m.text.lower())
+def command_photo(message):
 	#Replace by image path
 	bot.send_photo(m.chat.id, open('/home/r33int/Git/roucouleBot/jpp.jpg', 'rb'))
 
