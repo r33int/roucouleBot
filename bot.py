06a6bfdf98585514a4baba6b2a24b95816119f7c
r33int <r33intx@gmail.com>
2018-03-20 23:34:56 +0100

    add tg

--- bot.py
+++ bot.py
@@ -66,6 +66,10 @@ def command_text(message):
 def command_text(message):
     bot.send_message(message.chat.id, "Bonne nuit !");
 
+@bot.message_handler(func=lambda m: "tg" in m.text.lower())
+def command_text(message):
+    bot.send_message(message.chat.id, "tg");
+
 @bot.message_handler(func=lambda m: "jpp" in m.text.lower())
 def command_photo(message):
 	#Replace by image path
